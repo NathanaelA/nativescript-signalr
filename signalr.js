a6adbd4b6e91702c4e74bb3440da1dbd7bc0988d
Nathanael Anderson <nathan@master-technology.com>
2016-07-12 17:46:53 -0500

    Adding signalr.js

---
+++ signalr.js
@@ -0,0 +1,5 @@
+// This is a placeholder file until this is released as a MIT library
+//
+// You can get the full version by supporting me at http://patreon.com/NathanaelA or a commercial version from http://NativeScript.tools
+
+throw new Error("NativeScript-SignalR is not free module yet!   It is currently only available to those who support me at http://patreon.com/NathanaelA or under a commercial license at http://NativeScript.tools");
